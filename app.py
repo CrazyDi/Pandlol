07fe409416cb2a876ffa9e356cbad59b1fce8ae2
CrazyDi <43439123+CrazyDi@users.noreply.github.com>
2019-11-20 15:00:59 +0300

    config app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_restful import Api
+from dotenv import load_dotenv
+
 app = Flask(__name__, template_folder='www/public', static_folder='www/public/assets')
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+load_dotenv(".env", verbose=True)
+# app.config.from_object("default_config")
+# app.config.from_envvar("APPLICATION_SETTINGS")
+api = Api(app)
 
 
 @app.route('/')
