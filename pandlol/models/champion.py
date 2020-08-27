dfd1b6572b8c563d305c60c8540791a96a57b3bd
CrazyDi <43439123+CrazyDi@users.noreply.github.com>
2020-08-27 18:20:57 +0300

    champion tag upload

--- pandlol/models/champion.py
+++ pandlol/models/champion.py
@@ -11,7 +11,6 @@ class ChampionModel(db.Model):
     """
     Модель таблицы чемпионов
     """
-
     __tablename__ = "champion_list"
 
     champion_id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +19,59 @@ class ChampionModel(db.Model):
     def __init__(self, champion_id, champion_name):
         self.champion_id = champion_id
         self.champion_name = champion_name
+
+    @log_database_error(logger)
+    def save_to_db(self):
+        """
+        Сохранение записи в БД
+        """
+        db.session.add(self)
+        db.session.commit()
+        return None
+
+    @classmethod
+    @log_database_error(logger)
+    def delete_all_from_db(cls):
+        """
+        Удаление всех записей из таблицы
+        """
+        cls.query.delete()
+        db.session.commit()
+        return None
+
+
+class TagModel(db.Model):
+    """
+    Модель таблицы тегов
+    """
+    __tablename__ = "tag_list"
+
+    tag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    tag_name = db.Column(db.String(20), nullable=False, unique=True)
+
+    @classmethod
+    @log_database_error(logger)
+    def delete_all_from_db(cls):
+        """
+        Удаление всех записей из таблицы
+        """
+        cls.query.delete()
+        db.session.commit()
+        return None
+
+
+class ChampionTagModel(db.Model):
+    """
+    Модель таблицы тегов чемпионов
+    """
+    __tablename__ = "champion_tag"
+
+    champion_tag_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    champion_id = db.Column(db.Integer, db.ForeignKey('champion_list.champion_id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag_list.tag_id'))
+
+    champion = db.relationship(ChampionModel, backref=db.backref("champion_tag"))
+    tag = db.relationship(TagModel, backref=db.backref("champion_tag"))
 
     @log_database_error(logger)
     def save_to_db(self):
--- pandlol/upload.py
+++ pandlol/upload.py
@@ -2,13 +2,13 @@ from os import getcwd
 from sys import path
 path.insert(1, getcwd())
 
-from pandas import read_json
+from pandas import read_json, DataFrame, read_sql_table, merge
 from logging import getLogger
 
 from pandlol import db
 from pandlol.constant import url_versions, url_champions
 from pandlol.models.version import VersionModel
-from pandlol.models.champion import ChampionModel
+from pandlol.models.champion import ChampionModel, TagModel, ChampionTagModel
 from pandlol.utils import log_database_error
 
 
@@ -29,7 +29,6 @@ def upload_version(version):
     current_version.save_to_db()
 
 
-@log_database_error(logger)
 def upload_champion(version):
     """
     Загрузка чемпионов
@@ -39,10 +38,49 @@ def upload_champion(version):
     new_df.columns = ["champion_name", "champion_id"]
 
     # удаляем старые записи
+    ChampionTagModel.delete_all_from_db()
     ChampionModel.delete_all_from_db()
 
     # записываем новые записи
     new_df.to_sql("champion_list", db.engine, if_exists="append", index=False)
+
+
+def upload_tag(version):
+    """
+    Загрузка тегов
+    """
+    # читаем данные и преобразовываем их в нужный вид
+    tag_set = set()
+    read_json(url_champions.format(version))["data"].apply(lambda x: [tag_set.add(y) for y in x["tags"]])
+    tag_df = DataFrame({"tag_name": list(tag_set)})
+
+    # удаляем старые записи
+    TagModel.delete_all_from_db()
+
+    # записываем новые данные
+    tag_df.to_sql("tag_list", db.engine, if_exists="append", index=False)
+
+
+def upload_champion_tag(version):
+    """
+    Загрузка тегов чемпионов
+    """
+    # исходные данные
+    champion_df = read_json(url_champions.format(version))["data"]
+    tag_df = read_sql_table("tag_list", db.engine, index_col=["tag_name"])
+
+    # преобразовываем данные в нужный вид
+    champion_df = DataFrame(list(champion_df.values))[["key", "tags"]]
+    champion_df.columns = ["champion_id", "tags"]
+    champion_df = DataFrame(champion_df.tags.tolist(), index=champion_df.champion_id)\
+        .stack()\
+        .reset_index(level=1, drop=True)\
+        .reset_index(name='tags')
+    champion_tag_df = merge(champion_df, tag_df, left_on="tags", right_on="tag_name")
+    champion_tag_df = champion_tag_df[["champion_id", "tag_id"]]
+
+    # записываем данные в таблицу
+    champion_tag_df.to_sql("champion_tag", db.engine, if_exists="append", index=False)
 
 
 if __name__ == "__main__":
@@ -52,4 +90,6 @@ if __name__ == "__main__":
     if not last_version == VersionModel.current_version().version_code:
         # pass
         upload_champion(last_version)
+        upload_tag(last_version)
+        upload_champion_tag(last_version)
         upload_version(last_version)
