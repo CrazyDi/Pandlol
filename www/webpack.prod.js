73f176c858a7b0d7f3b0fb7e52d2fd54f1da7191
Yury Chernyavsky <yury.chernyavsky@solbeg.com>
2021-01-01 16:16:25 +0300

    [F-10] Added TypeScript configuration for the application (dev + prod)

--- www/webpack.prod.js
+++ www/webpack.prod.js
@@ -1,99 +1,73 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'production',
     entry: {
-        main: './src/index.js'
+        main: './src/index.tsx'
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'js/[hash].js'
+        filename: 'assets/[fullhash].js',
+        publicPath: '/'
     },
     module: {
         rules: [
             {
-                test: /\.(js)$/,
+                test: /\.(ts|tsx|js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: [
                             '@babel/preset-env',
-                            '@babel/preset-react'
-                        ],
-                        plugins: [
-                            '@babel/plugin-transform-arrow-functions',
-                            '@babel/plugin-proposal-class-properties'
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
                         ]
                     }
                 }
             },
             {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
-                test: /\.(jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            name: '/[path][name].[ext]',
-                            outputPath: '../',
-                            publicPath: (url) => {
-                                return `${url}`;
-                            }
-                        }
-                    }
-                ]
-            },
-
-            {
-                test: /\.(png|svg)$/,
+                test: /\.(png|jpg|svg)$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
-                        name: '/[path][name].[ext]',
-                        outputPath: '../',
-                        publicPath: (url) => {
-                            return `${url}`;
-                        }
+                        name: '[name].[ext]',
+                        outputPath: '/assets/icons/'
                     }
                 }]
             },
-
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
-                        name: '/[path][name].[ext]',
-                        outputPath: '../',
-                        publicPath: (url) => {
-                            return `${url}`;
-                        }
+                        name: '[name].[ext]',
+                        outputPath: '/assets/fonts/'
                     }
                 }]
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: 'css/[hash].css'
-        }),
-        new CleanWebpackPlugin()
+            filename: 'assets/[fullhash].css'
+        })
     ],
     resolve: {
-        extensions: ['*', '.js'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '*'],
         alias: {
             app: path.resolve(__dirname, 'src/app/')
         }
     }
-};
+}
